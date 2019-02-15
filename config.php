90e5751732d29c726f714bf2a036b541d24e0a7e
wowoinhfut <l_wowo@sina.com>
2019-02-15 13:44:27 +0800

    ocr

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * User: guizheng@iflytek.com
+ * Date: 2019/2/15 上午9:49
+ */
+$config = [
+    // 设置文件保存路径
+    'upload_path' => '/usr/local/var/www/aireading/upload',
+
+    // appid
+    'appid' => '5c661a16',
+
+    // ocr
+    'ocr_api' => 'http://webapi.xfyun.cn/v1/service/v1/ocr/general',
+    'ocr_key' => '373ca8185a9b4013a407b1a55561662f',
+
+    // tts app key
+    'tts_api' => '',
+    'tts_key' => '8ad63b0e47f63909a9b8cd33a4ad43f1',
+];
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-// 设置文件保存路径
-$upload_path = '/usr/local/var/www/aireading/upload';
+include "config.php";
+
+if (!isset($config)) {
+    die('配置错误。');
+}
 
 // 检查上传文件
 if (empty($_FILES['input_image'])) {
@@ -14,7 +17,51 @@ if ($_FILES['input_image']['size'] > 3145728) {
 
 // 上传文件
 $uploaded_path = $_FILES['input_image']['tmp_name'];
-$save_path = $upload_path . '/' . time() . '.jpeg';
+$save_path = $config['upload_path'] . '/' . time() . '.jpeg';
 move_uploaded_file($uploaded_path, $save_path);
 
+echo ocrRequest($save_path);
+exit;
+
+// ocr
+
+// tts
+
 // 存数据库
+
+function ocrRequest($pic_path)
+{
+    global $config;
+    $xParam = base64_encode(json_encode(array(
+        "language" => "cn|en",
+        "location" => "false",
+    )));
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $config['ocr_api']);
+    $curTime = time();
+    $headers = [
+        'X-Appid:' . $config['appid'],
+        'X-CurTime:' . $curTime,
+        'X-Param:' . $xParam,
+        'X-CheckSum:' . md5($config['ocr_key'] . $curTime . $xParam),
+        'Content-Type:application/x-www-form-urlencoded; charset=utf-8'
+    ];
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+        'image' => base64EncodeImage($pic_path)
+    ]));
+
+    $data = curl_exec($ch);
+    return $data;
+}
+
+function base64EncodeImage($image_file)
+{
+    $image_data = file_get_contents($image_file);
+    $base64_image = base64_encode($image_data);
+    return $base64_image;
+}
